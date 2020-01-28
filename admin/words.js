d2783d790d6bf6a8ef5b54a4accfc334bd185ac7
mrbungle64 <mrb1232@posteo.de>
2020-01-28 22:25:14 +0100

    Translation of all relevant texts into all needed languages (https://translator.iobroker.in/)

--- admin/words.js
+++ admin/words.js
@@ -6,34 +6,98 @@
 systemDictionary = {
     'template adapter settings': {
         'en': 'Adapter settings',
-        'de': 'Adaptereinstellungen'
+        'de': 'Adaptereinstellungen',
+        'ru': 'Настройки адаптера',
+        'pt': 'Configurações do adaptador',
+        'nl': 'Adapter instellingen',
+        'fr': 'Paramètres de l\'adaptateur',
+        'it': 'Impostazioni dell\'adattatore',
+        'es': 'Ajustes del adaptador',
+        'pl': 'Ustawienia adaptera',
+        'zh-cn': '适配器设定'
     },
     'email': {
         'en': 'Email',
-        'de': 'Email'
+        'de': 'Email',
+        'ru': 'Эл. адрес',
+        'pt': 'O email',
+        'nl': 'E-mail',
+        'fr': 'Email',
+        'it': 'E-mail',
+        'es': 'Email',
+        'pl': 'E-mail',
+        'zh-cn': '电子邮件'
     },
     'password': {
         'en': 'Password',
-        'de': 'Passwort'
+        'de': 'Passwort',
+        'ru': 'пароль',
+        'pt': 'Senha',
+        'nl': 'Wachtwoord',
+        'fr': 'Mot de passe',
+        'it': 'Parola d\'ordine',
+        'es': 'Contraseña',
+        'pl': 'Hasło',
+        'zh-cn': '密码'
     },
     'countrycode': {
         'en': 'Country code (continent)',
-        'de': 'Ländercode (Kontinent)'
+        'de': 'Ländercode (Kontinent)',
+        'ru': 'Код страны (континент)',
+        'pt': 'Código do país (continente)',
+        'nl': 'Landcode (continent)',
+        'fr': 'Code du pays (continent)',
+        'it': 'Codice paese (continente)',
+        'es': 'Código de país (continente)',
+        'pl': 'Kod kraju (kontynent)',
+        'zh-cn': '国家/地区代码（大陆）'
     },
     'deviceNumber': {
         'en': 'Device number',
-        'de': 'Gerät Nr.'
+        'de': 'Gerät Nr.',
+        'ru': 'Номер устройства',
+        'pt': 'Número do dispositivo',
+        'nl': 'Apparaatnummer',
+        'fr': 'Numéro d\'appareil',
+        'it': 'Numero del dispositivo',
+        'es': 'Número del dispositivo',
+        'pl': 'Numer urządzenia',
+        'zh-cn': '设备编号'
     },
     'maxautoretries': {
         'en': 'Max. auto retries if connection fails',
-        'de': 'Max. Versuche wenn die Verbindung fehlschlägt'
+        'de': 'Max. automatische Versuche, wenn die Verbindung fehlschlägt',
+        'ru': 'Максимум. автоматическая повторная попытка в случае сбоя соединения',
+        'pt': 'Máx. tentativas automáticas se a conexão falhar',
+        'nl': 'Max. automatisch opnieuw proberen als verbinding mislukt',
+        'fr': 'Max. tentatives automatiques en cas d\'échec de la connexion',
+        'it': 'Max. riprova automaticamente se la connessione fallisce',
+        'es': 'Max. reintentos automáticos si falla la conexión',
+        'pl': 'Max. automatyczne ponawianie, jeśli połączenie się nie powiedzie',
+        'zh-cn': '最高连接失败时自动重试'
     },
     'retrypause': {
         'en': 'Time period for retry (seconds)',
-        'de': 'Wartezeit bis zum nächsten Versuch (Sekunden)'
+        'de': 'Wartezeit bis zum nächsten Versuch (Sekunden)',
+        'ru': 'Период времени для повторения (секунд)',
+        'pt': 'Período de tempo para nova tentativa (segundos)',
+        'nl': 'Tijdsperiode voor opnieuw proberen (seconden)',
+        'fr': 'Délai pour réessayer (secondes)',
+        'it': 'Periodo di tempo per riprovare (secondi)',
+        'es': 'Período de tiempo para reintentar (segundos)',
+        'pl': 'Okres ponownej próby (sekundy)',
+        'zh-cn': '重试时间（秒）'
     },
     'numberOfSpotAreas': {
         'en': 'number of spot areas',
-        'de': 'Anzahl von Bereichen'
+        'de': 'Anzahl von Bereichen',
+        'ru': 'количество площадок',
+        'pt': 'número de áreas pontuais',
+        'nl': 'aantal plekgebieden',
+        'fr': 'nombre de zones ponctuelles',
+        'it': 'numero di aree spot',
+        'es': 'cantidad de áreas puntuales',
+        'pl': 'liczba miejsc dodatkowych',
+        'zh-cn': '点数'
     }
 };
