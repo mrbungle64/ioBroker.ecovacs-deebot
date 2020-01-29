709e9074d4d29c1012a765365e97cddc251653f6
mrbungle64 <mrb1232@posteo.de>
2020-01-29 20:03:06 +0100

    Switch to quotes

--- admin/words.js
+++ admin/words.js
@@ -1,67 +1,68 @@
+/* eslint-disable quotes */
 // @ts-nocheck
 // eslint-disable-next-line no-unused-vars
 /*global systemDictionary:true */
 'use strict';
 
 systemDictionary = {
-    'email': {
-        'en': 'Email',
-        'de': 'Email',
-        'ru': 'Эл. адрес',
-        'pt': 'O email',
-        'nl': 'E-mail',
-        'fr': 'Email',
-        'it': 'E-mail',
-        'es': 'Email',
-        'pl': 'E-mail',
-        'zh-cn': '电子邮件'
+    "email": {
+        "en": "Email",
+        "de": "Email",
+        "ru": "Эл. адрес",
+        "pt": "O email",
+        "nl": "E-mail",
+        "fr": "Email",
+        "it": "E-mail",
+        "es": "Email",
+        "pl": "E-mail",
+        "zh-cn": "电子邮件"
     },
-    'password': {
-        'en': 'Password',
-        'de': 'Passwort',
-        'ru': 'пароль',
-        'pt': 'Senha',
-        'nl': 'Wachtwoord',
-        'fr': 'Mot de passe',
-        'it': 'Parola d\'ordine',
-        'es': 'Contraseña',
-        'pl': 'Hasło',
-        'zh-cn': '密码'
+    "password": {
+        "en": "Password",
+        "de": "Passwort",
+        "ru": "пароль",
+        "pt": "Senha",
+        "nl": "Wachtwoord",
+        "fr": "Mot de passe",
+        "it": "Parola d'ordine",
+        "es": "Contraseña",
+        "pl": "Hasło",
+        "zh-cn": "密码"
     },
-    'countrycode': {
-        'en': 'Country code (continent)',
-        'de': 'Ländercode (Kontinent)',
-        'ru': 'Код страны (континент)',
-        'pt': 'Código do país (continente)',
-        'nl': 'Landcode (continent)',
-        'fr': 'Code du pays (continent)',
-        'it': 'Codice paese (continente)',
-        'es': 'Código de país (continente)',
-        'pl': 'Kod kraju (kontynent)',
-        'zh-cn': '国家/地区代码（大陆）'
+    "countrycode": {
+        "en": "Country code (continent)",
+        "de": "Ländercode (Kontinent)",
+        "ru": "Код страны (континент)",
+        "pt": "Código do país (continente)",
+        "nl": "Landcode (continent)",
+        "fr": "Code du pays (continent)",
+        "it": "Codice paese (continente)",
+        "es": "Código de país (continente)",
+        "pl": "Kod kraju (kontynent)",
+        "zh-cn": "国家/地区代码（大陆）"
     },
-    'deviceNumber': {
-        'en': 'Device number',
-        'de': 'Gerät Nr.',
-        'ru': 'Номер устройства',
-        'pt': 'Número do dispositivo',
-        'nl': 'Apparaatnummer',
-        'fr': 'Numéro d\'appareil',
-        'it': 'Numero del dispositivo',
-        'es': 'Número del dispositivo',
-        'pl': 'Numer urządzenia',
-        'zh-cn': '设备编号'
+    "deviceNumber": {
+        "en": "Device number",
+        "de": "Gerät Nr.",
+        "ru": "Номер устройства",
+        "pt": "Número do dispositivo",
+        "nl": "Apparaatnummer",
+        "fr": "Numéro d'appareil",
+        "it": "Numero del dispositivo",
+        "es": "Número del dispositivo",
+        "pl": "Numer urządzenia",
+        "zh-cn": "设备编号"
     },
-    'numberOfSpotAreas': {
-        'en': 'number of spot areas',
-        'de': 'Anzahl von Bereichen',
-        'ru': 'количество площадок',
-        'pt': 'número de áreas pontuais',
-        'nl': 'aantal plekgebieden',
-        'fr': 'nombre de zones ponctuelles',
-        'it': 'numero di aree spot',
-        'es': 'cantidad de áreas puntuales',
-        'pl': 'liczba miejsc dodatkowych',
-        'zh-cn': '点数'
+    "numberOfSpotAreas": {
+        "en": "number of spot areas",
+        "de": "Anzahl von Bereichen",
+        "ru": "количество площадок",
+        "pt": "número de áreas pontuais",
+        "nl": "aantal plekgebieden",
+        "fr": "nombre de zones ponctuelles",
+        "it": "numero di aree spot",
+        "es": "cantidad de áreas puntuales",
+        "pl": "liczba miejsc dodatkowych",
+        "zh-cn": "点数"
     }
 };
