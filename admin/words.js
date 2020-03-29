7aa41c0191a7480abfaebeef90fe7bfd343be805
mrbungle64 <mrb1232@posteo.de>
2020-03-29 22:27:46 +0200

    Show warning message for options to enable features

--- admin/words.js
+++ admin/words.js
@@ -100,5 +100,9 @@ systemDictionary = {
     "feature.control.playIamHere": {
         "en": "Voice report for locating the device",
         "de": "Sprachbericht zur Lokalisierung des Roboters"
+    },
+    "featuresWarning": {
+        "en": "Warning: Unlocking of functions is at your own risk. The implementation of functions varies from model to model and some functions are not available in every model.",
+        "de": "Warnung: Das Freischalten von Funktionen geschieht auf eigene Gefahr. Die Implementierungen von Funktionen sind von Modell zu Modell unterschiedlich und einige Funktionen sind nicht bei jedem Modell verfügbar."
     }
 };
