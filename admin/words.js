1ca6937a78140ad73e7d9ffbd67e8d10165dd749
mrbungle64 <mrb1232@posteo.de>
2020-05-21 19:11:10 +0200

    Some translations added

--- admin/words.js
+++ admin/words.js
@@ -67,66 +67,194 @@ systemDictionary = {
     },
     "pre-selection": {
         "en": "pre-selection",
-        "de": "Vorauswahl verwenden"
+        "de": "Vorauswahl verwenden",
+        "ru": "предварительный отбор",
+        "pt": "pré-seleção",
+        "nl": "voorselectie",
+        "fr": "présélection",
+        "it": "preselezione",
+        "es": "preselección",
+        "pl": "preselekcja",
+        "zh-cn": "预选"
     },
     "enable": {
         "en": "enable",
-        "de": "aktivieren"
+        "de": "aktivieren",
+        "ru": "включить",
+        "pt": "habilitar",
+        "nl": "inschakelen",
+        "fr": "activer",
+        "it": "abilitare",
+        "es": "habilitar",
+        "pl": "włączyć",
+        "zh-cn": "使能"
     },
     "areas": {
         "en": "areas",
-        "de": "Bereiche"
+        "de": "Bereiche",
+        "ru": "районы",
+        "pt": "áreas",
+        "nl": "gebieden",
+        "fr": "domaines",
+        "it": "le zone",
+        "es": "áreas",
+        "pl": "obszary",
+        "zh-cn": "地区"
     },
     "other ...": {
         "en": "other ...",
-        "de": "weiteres ..."
+        "de": "weiteres ...",
+        "ru": "other ...",
+        "pt": "other ...",
+        "nl": "other ...",
+        "fr": "other ...",
+        "it": "other ...",
+        "es": "other ...",
+        "pl": "other ...",
+        "zh-cn": "other ..."
     },
     "disable": {
         "en": "disabled",
-        "de": "deaktivieren"
+        "de": "deaktivieren",
+        "ru": "отключенной",
+        "pt": "desativado",
+        "nl": "gedeactiveerd",
+        "fr": "désactivé",
+        "it": "disattivato",
+        "es": "desactivado",
+        "pl": "wyłączony",
+        "zh-cn": "停用"
     },
     "feature.cleaninglog.channel": {
         "en": "Display various cleaninglog values",
-        "de": "Zeige sämtliche Werte vom Reinigungsprotokoll"
+        "de": "Zeige sämtliche Werte vom Reinigungsprotokoll",
+        "ru": "Показать различные значения журнала очистки",
+        "pt": "Exibir vários valores do log de limpeza",
+        "nl": "Geef verschillende schoonmaaklog-waarden weer",
+        "fr": "Afficher diverses valeurs de journal de nettoyage",
+        "it": "Visualizza vari valori di pulizia",
+        "es": "Mostrar varios valores de registro de limpieza",
+        "pl": "Wyświetlaj różne wartości dziennika czyszczenia",
+        "zh-cn": "显示各种清洁日志值"
     },
     "feature.info.dustbox": {
         "en": "Display dustbox status",
-        "de": "Zeige an, ob die Staubbox eingesetzt ist"
+        "de": "Zeige an, ob die Staubbox eingesetzt ist",
+        "ru": "Отображение статуса мусорной коробки",
+        "pt": "Exibir status da caixa de poeira",
+        "nl": "Geef de status van de stofbak weer",
+        "fr": "Afficher l'état du bac à poussière",
+        "it": "Visualizza lo stato del cestino",
+        "es": "Mostrar el estado de la caja de polvo",
+        "pl": "Wyświetl stan pojemnika na śmieci",
+        "zh-cn": "显示垃圾箱状态"
     },
     "feature.control.pause": {
         "en": "Pause function",
-        "de": "Pause Funktion"
+        "de": "Pause Funktion",
+        "ru": "Функция паузы",
+        "pt": "Função de pausa",
+        "nl": "Pauze functie",
+        "fr": "Fonction pause",
+        "it": "Funzione di pausa",
+        "es": "Función de pausa",
+        "pl": "Funkcja pauzy",
+        "zh-cn": "暂停功能"
     },
     "feature.control.resume": {
         "en": "Resume function",
-        "de": "Fortsetzen Funktion"
+        "de": "Fortsetzen Funktion",
+        "ru": "Возобновить функцию",
+        "pt": "Retomar a função",
+        "nl": "Hervat functie",
+        "fr": "Fonction de reprise",
+        "it": "Riprendi funzione",
+        "es": "Reanudar la función",
+        "pl": "Wznów funkcję",
+        "zh-cn": "恢复功能"
     },
     "feature.control.playSound": {
         "en": "Play a sound for locating the device",
-        "de": "Sound abspielen zur Lokalisierung des Roboters"
+        "de": "Sound abspielen zur Lokalisierung des Roboters",
+        "ru": "Воспроизвести звук для определения местоположения устройства",
+        "pt": "Tocar um som para localizar o dispositivo",
+        "nl": "Speel een geluid af om het apparaat te lokaliseren",
+        "fr": "Émettre un son pour localiser l'appareil",
+        "it": "Riproduci un suono per localizzare il dispositivo",
+        "es": "Reproduzca un sonido para localizar el dispositivo.",
+        "pl": "Odtwórz dźwięk, aby zlokalizować urządzenie",
+        "zh-cn": "播放声音以查找设备"
     },
     "feature.control.playIamHere": {
         "en": "Voice report for locating the device",
-        "de": "Sprachbericht zur Lokalisierung des Roboters"
+        "de": "Sprachbericht zur Lokalisierung des Roboters",
+        "ru": "Голосовой отчет для определения местоположения устройства",
+        "pt": "Relatório de voz para localizar o dispositivo",
+        "nl": "Stemrapport voor het lokaliseren van het apparaat",
+        "fr": "Rapport vocal pour localiser l'appareil",
+        "it": "Rapporto vocale per l'individuazione del dispositivo",
+        "es": "Informe de voz para localizar el dispositivo.",
+        "pl": "Raport głosowy dotyczący lokalizacji urządzenia",
+        "zh-cn": "语音报告，用于定位设备"
     },
     "feature.control.move": {
         "en": "Manually moving around",
-        "de": "Manuelles steuern"
+        "de": "Manuelles steuern",
+        "ru": "Перемещение вручную",
+        "pt": "Movendo-se manualmente",
+        "nl": "Handmatig bewegen",
+        "fr": "Se déplacer manuellement",
+        "it": "Muoversi manualmente",
+        "es": "Moviéndose manualmente",
+        "pl": "Ręczne poruszanie się",
+        "zh-cn": "手动移动"
     },
     "featuresWarning": {
         "en": "Warning: Unlocking of functions is at your own risk. The implementation of functions varies from model to model and some functions are not available in every model.",
-        "de": "Warnung: Das Freischalten von Funktionen geschieht auf eigene Gefahr. Die Implementierungen von Funktionen sind von Modell zu Modell unterschiedlich und einige Funktionen sind nicht bei jedem Modell verfügbar."
+        "de": "Warnung: Das Freischalten von Funktionen geschieht auf eigene Gefahr. Die Implementierungen von Funktionen sind von Modell zu Modell unterschiedlich und einige Funktionen sind nicht bei jedem Modell verfügbar.",
+        "ru": "Предупреждение: разблокировка функций на свой страх и риск. Реализация функций варьируется от модели к модели, и некоторые функции доступны не в каждой модели.",
+        "pt": "Aviso: O desbloqueio de funções é por sua conta e risco. A implementação de funções varia de modelo para modelo e algumas funções não estão disponíveis em todos os modelos.",
+        "nl": "Waarschuwing: het ontgrendelen van functies is op eigen risico. De implementatie van functies verschilt van model tot model en sommige functies zijn niet in elk model beschikbaar.",
+        "fr": "Avertissement: le déverrouillage des fonctions est à vos risques et périls. L'implémentation des fonctions varie d'un modèle à l'autre et certaines fonctions ne sont pas disponibles dans tous les modèles.",
+        "it": "Avvertenza: lo sblocco delle funzioni è a proprio rischio. L'implementazione delle funzioni varia da modello a modello e alcune funzioni non sono disponibili in tutti i modelli.",
+        "es": "Advertencia: el desbloqueo de funciones es bajo su propio riesgo. La implementación de funciones varía de un modelo a otro y algunas funciones no están disponibles en todos los modelos.",
+        "pl": "Ostrzeżenie: Odblokowanie funkcji odbywa się na własne ryzyko. Implementacja funkcji różni się w zależności od modelu, a niektóre funkcje nie są dostępne w każdym modelu.",
+        "zh-cn": "警告：解锁功能需要您自担风险。函数的实现因模型而异，并且某些函数并非在每个模型中都可用。"
     },
     "workaround.batteryValue": {
         "en": "Enable workaround for weird battery values (e.g. on Deebot 900/901)",
-        "de": "Workaround für merkwürdigen Verlauf der Akkuwerte (z.B. beim Deebot 900/901)"
+        "de": "Workaround für merkwürdigen Verlauf der Akkuwerte (z.B. beim Deebot 900/901)",
+        "ru": "Включить обходной путь для странных значений батареи (например, на Deebot 900/901)",
+        "pt": "Ative a solução alternativa para valores estranhos da bateria (por exemplo, no Deebot 900/901)",
+        "nl": "Tijdelijke oplossing inschakelen voor rare batterijwaarden (bijvoorbeeld op Deebot 900/901)",
+        "fr": "Activer la solution de contournement pour les valeurs de batterie étranges (par exemple sur Deebot 900/901)",
+        "it": "Abilita soluzione alternativa per strani valori della batteria (ad es. Su Deebot 900/901)",
+        "es": "Habilite la solución alternativa para valores de batería extraños (por ejemplo, en Deebot 900/901)",
+        "pl": "Włącz obejście dziwnych wartości baterii (np. W Deebot 900/901)",
+        "zh-cn": "启用针对电池电量异常的解决方法（例如在Deebot 900/901上）"
     },
     "workaround.lastCleaningAPICall": {
         "en": "Use alternative API call for lastCleaningMapImageURL and lastCleaningTimestamp (Ozmo 920/950)",
-        "de": "Alternativen API Befehl für lastCleaningMapImageURL und lastCleaningTimestamp nutzen (Ozmo 920/950)"
+        "de": "Alternativen API Befehl für lastCleaningMapImageURL und lastCleaningTimestamp nutzen (Ozmo 920/950)",
+        "ru": "Используйте альтернативный вызов API для lastCleaningMapImageURL и lastCleaningTimestamp (Ozmo 920/950)",
+        "pt": "Use a chamada de API alternativa para lastCleaningMapImageURL e lastCleaningTimestamp (Ozmo 920/950)",
+        "nl": "Gebruik een alternatieve API-aanroep voor lastCleaningMapImageURL en lastCleaningTimestamp (Ozmo 920/950)",
+        "fr": "Utiliser un autre appel d'API pour lastCleaningMapImageURL et lastCleaningTimestamp (Ozmo 920/950)",
+        "it": "Utilizza una chiamata API alternativa per lastCleaningMapImageURL e lastCleaningTimestamp (Ozmo 920/950)",
+        "es": "Utilice una llamada API alternativa para lastCleaningMapImageURL y lastCleaningTimestamp (Ozmo 920/950)",
+        "pl": "Użyj alternatywnego wywołania API dla lastCleaningMapImageURL i lastCleaningTimestamp (Ozmo 920/950)",
+        "zh-cn": "对lastCleaningMapImageURL和lastCleaningTimestamp使用替代API调用（Ozmo 920/950）"
     },
     "pollingInterval": {
         "en": "Polling interval (seconds)",
-        "de": "Abfrage Intervall (Sekunden)"
+        "de": "Abfrage Intervall (Sekunden)",
+        "ru": "Интервал опроса (секунды)",
+        "pt": "Intervalo de pesquisa (segundos)",
+        "nl": "Polling-interval (seconden)",
+        "fr": "Intervalle d'interrogation (secondes)",
+        "it": "Intervallo di polling (secondi)",
+        "es": "Intervalo de sondeo (segundos)",
+        "pl": "Interwał odpytywania (sekundy)",
+        "zh-cn": "轮询间隔（秒）"
     }
 };
