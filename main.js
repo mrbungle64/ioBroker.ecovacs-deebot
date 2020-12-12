04bc72c94c99627b0c4467900600b5111a1af9b6
mrbungle64 <mrb1232@posteo.de>
2020-12-12 19:20:07 +0100

    Empty control.goToPosition when the position has been reached

--- main.js
+++ main.js
@@ -674,6 +674,7 @@ class EcovacsDeebot extends utils.Adapter {
                         if (this.goToPositionArea) {
                             if (mapHelper.positionIsInAreaValueString(x, y, this.goToPositionArea)) {
                                 this.vacbot.run('pause');
+                                this.setStateConditional('control.goToPosition', '', true);
                                 this.goToPositionArea = null;
                             }
                         }
