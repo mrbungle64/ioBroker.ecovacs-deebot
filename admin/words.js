eb5692a98fd8656e4f1b8c1317623fc70fc065fb
mrbungle64 <mrb1232@posteo.de>
2021-01-01 22:40:03 +0100

    Some improvements to adapter config

--- admin/words.js
+++ admin/words.js
@@ -7,67 +7,27 @@
 systemDictionary = {
     "email": {
         "en": "Email",
-        "de": "Email",
-        "ru": "Эл. адрес",
-        "pt": "O email",
-        "nl": "E-mail",
-        "fr": "Email",
-        "it": "E-mail",
-        "es": "Email",
-        "pl": "E-mail",
-        "zh-cn": "电子邮件"
+        "de": "Email"
     },
     "password": {
         "en": "Password",
-        "de": "Passwort",
-        "ru": "пароль",
-        "pt": "Senha",
-        "nl": "Wachtwoord",
-        "fr": "Mot de passe",
-        "it": "Parola d'ordine",
-        "es": "Contraseña",
-        "pl": "Hasło",
-        "zh-cn": "密码"
+        "de": "Passwort"
     },
     "countrycode": {
         "en": "Country code (continent)",
-        "de": "Ländercode (Kontinent)",
-        "ru": "Код страны (континент)",
-        "pt": "Código do país (continente)",
-        "nl": "Landcode (continent)",
-        "fr": "Code du pays (continent)",
-        "it": "Codice paese (continente)",
-        "es": "Código de país (continente)",
-        "pl": "Kod kraju (kontynent)",
-        "zh-cn": "国家/地区代码（大陆）"
+        "de": "Ländercode (Kontinent)"
     },
     "deviceNumber": {
         "en": "Device number",
-        "de": "Gerät Nr.",
-        "ru": "Номер устройства",
-        "pt": "Número do dispositivo",
-        "nl": "Apparaatnummer",
-        "fr": "Numéro d'appareil",
-        "it": "Numero del dispositivo",
-        "es": "Número del dispositivo",
-        "pl": "Numer urządzenia",
-        "zh-cn": "设备编号"
+        "de": "Gerät Nr."
     },
     "numberOfSpotAreas": {
         "en": "number of spot areas",
-        "de": "Anzahl von Bereichen",
-        "ru": "количество площадок",
-        "pt": "número de áreas pontuais",
-        "nl": "aantal plekgebieden",
-        "fr": "nombre de zones ponctuelles",
-        "it": "numero di aree spot",
-        "es": "cantidad de áreas puntuales",
-        "pl": "liczba miejsc dodatkowych",
-        "zh-cn": "点数"
+        "de": "Anzahl von Bereichen"
     },
     "languageForSpotAreaNames": {
-        "en": "language for spot area names (920/950/T8)",
-        "de": "Sprache für die Namen der Bereiche (920/950/T8)"
+        "en": "language for spot area names (OZMO 920/950/T8)",
+        "de": "Sprache für die Namen der Bereiche (OZMO 920/950/T8)"
     },
     "english": {
         "en": "English",
@@ -79,159 +39,59 @@ systemDictionary = {
     },
     "pre-selection": {
         "en": "pre-selection",
-        "de": "Vorauswahl verwenden",
-        "ru": "предварительный отбор",
-        "pt": "pré-seleção",
-        "nl": "voorselectie",
-        "fr": "présélection",
-        "it": "preselezione",
-        "es": "preselección",
-        "pl": "preselekcja",
-        "zh-cn": "预选"
+        "de": "Vorauswahl verwenden"
     },
     "enable": {
         "en": "enable",
-        "de": "aktivieren",
-        "ru": "включить",
-        "pt": "habilitar",
-        "nl": "inschakelen",
-        "fr": "activer",
-        "it": "abilitare",
-        "es": "habilitar",
-        "pl": "włączyć",
-        "zh-cn": "使能"
+        "de": "aktivieren"
     },
     "areas": {
         "en": "areas",
-        "de": "Bereiche",
-        "ru": "районы",
-        "pt": "áreas",
-        "nl": "gebieden",
-        "fr": "domaines",
-        "it": "le zone",
-        "es": "áreas",
-        "pl": "obszary",
-        "zh-cn": "地区"
+        "de": "Bereiche"
+    },
+    "standardFunctions": {
+        "en": "Standard functions",
+        "de": "Standardfunktionen"
     },
     "other ...": {
         "en": "other ...",
-        "de": "weiteres ...",
-        "ru": "other ...",
-        "pt": "other ...",
-        "nl": "other ...",
-        "fr": "other ...",
-        "it": "other ...",
-        "es": "other ...",
-        "pl": "other ...",
-        "zh-cn": "other ..."
+        "de": "weiteres ..."
     },
     "disable": {
         "en": "disabled",
-        "de": "deaktivieren",
-        "ru": "отключенной",
-        "pt": "desativado",
-        "nl": "gedeactiveerd",
-        "fr": "désactivé",
-        "it": "disattivato",
-        "es": "desactivado",
-        "pl": "wyłączony",
-        "zh-cn": "停用"
+        "de": "deaktivieren"
     },
     "feature.cleaninglog.channel": {
         "en": "Display various cleaninglog values (cleaninglog)",
-        "de": "Zeige sämtliche Werte vom Reinigungsprotokoll (cleaninglog)",
-        "ru": "Показать различные значения журнала очистки (cleaninglog)",
-        "pt": "Exibir vários valores do log de limpeza (cleaninglog)",
-        "nl": "Geef verschillende schoonmaaklog-waarden weer (cleaninglog)",
-        "fr": "Afficher diverses valeurs de journal de nettoyage (cleaninglog)",
-        "it": "Visualizza vari valori di pulizia (cleaninglog)",
-        "es": "Mostrar varios valores de registro de limpieza (cleaninglog)",
-        "pl": "Wyświetlaj różne wartości dziennika czyszczenia (cleaninglog)",
-        "zh-cn": "显示各种清洁日志值 (cleaninglog)"
+        "de": "Zeige sämtliche Werte vom Reinigungsprotokoll (cleaninglog)"
     },
     "feature.info.dustbox": {
         "en": "Display dustbox status (info.dustbox)",
-        "de": "Zeige an, ob die Staubbox eingesetzt ist (info.dustbox)",
-        "ru": "Отображение статуса мусорной коробки (info.dustbox)",
-        "pt": "Exibir status da caixa de poeira (info.dustbox)",
-        "nl": "Geef de status van de stofbak weer (info.dustbox)",
-        "fr": "Afficher l'état du bac à poussière (info.dustbox)",
-        "it": "Visualizza lo stato del cestino (info.dustbox)",
-        "es": "Mostrar el estado de la caja de polvo (info.dustbox)",
-        "pl": "Wyświetl stan pojemnika na śmieci (info.dustbox)",
-        "zh-cn": "显示垃圾箱状态 (info.dustbox)"
+        "de": "Zeige an, ob die Staubbox eingesetzt ist (info.dustbox)"
     },
     "feature.control.pause": {
         "en": "Pause function (control.pause)",
-        "de": "Pause-Funktion (control.pause)",
-        "ru": "Функция паузы (control.pause)",
-        "pt": "Função de pausa (control.pause)",
-        "nl": "Pauze functie (control.pause)",
-        "fr": "Fonction pause (control.pause)",
-        "it": "Funzione di pausa (control.pause)",
-        "es": "Función de pausa (control.pause)",
-        "pl": "Funkcja pauzy (control.pause)",
-        "zh-cn": "暂停功能 (control.pause)"
+        "de": "Pause-Funktion (control.pause)"
     },
     "feature.control.resume": {
         "en": "Resume function (control.resume)",
-        "de": "Fortsetzen-Funktion (control.resume)",
-        "ru": "Возобновить функцию (control.resume)",
-        "pt": "Retomar a função (control.resume)",
-        "nl": "Hervat functie (control.resume)",
-        "fr": "Fonction de reprise (control.resume)",
-        "it": "Riprendi funzione (control.resume)",
-        "es": "Reanudar la función (control.resume)",
-        "pl": "Wznów funkcję (control.resume)",
-        "zh-cn": "恢复功能 (control.resume)"
+        "de": "Fortsetzen-Funktion (control.resume)"
     },
     "feature.control.playSound": {
         "en": "Play a sound for locating the device (control.playSound)",
-        "de": "Sound abspielen zur Lokalisierung des Roboters (control.playSound)",
-        "ru": "Воспроизвести звук для определения местоположения устройства (control.playSound)",
-        "pt": "Tocar um som para localizar o dispositivo (control.playSound)",
-        "nl": "Speel een geluid af om het apparaat te lokaliseren (control.playSound)",
-        "fr": "Émettre un son pour localiser l'appareil (control.playSound)",
-        "it": "Riproduci un suono per localizzare il dispositivo (control.playSound)",
-        "es": "Reproduzca un sonido para localizar el dispositivo. (control.playSound)",
-        "pl": "Odtwórz dźwięk, aby zlokalizować urządzenie (control.playSound)",
-        "zh-cn": "播放声音以查找设备 (control.playSound)"
+        "de": "Sound abspielen zur Lokalisierung des Roboters (control.playSound)"
     },
     "feature.control.playIamHere": {
         "en": "Voice report for locating the device (control.playIamHere)",
-        "de": "Sprachbericht zur Lokalisierung des Roboters (control.playIamHere)",
-        "ru": "Голосовой отчет для определения местоположения устройства (control.playIamHere)",
-        "pt": "Relatório de voz para localizar o dispositivo (control.playIamHere)",
-        "nl": "Stemrapport voor het lokaliseren van het apparaat (control.playIamHere)",
-        "fr": "Rapport vocal pour localiser l'appareil (control.playIamHere)",
-        "it": "Rapporto vocale per l'individuazione del dispositivo (control.playIamHere)",
-        "es": "Informe de voz para localizar el dispositivo. (control.playIamHere)",
-        "pl": "Raport głosowy dotyczący lokalizacji urządzenia (control.playIamHere)",
-        "zh-cn": "语音报告，用于定位设备 (control.playIamHere)"
+        "de": "Sprachbericht zur Lokalisierung des Roboters (control.playIamHere)"
     },
     "feature.control.move": {
         "en": "Manually moving around (control.move)",
-        "de": "Manuelles steuern (control.move)",
-        "ru": "Перемещение вручную (control.move)",
-        "pt": "Movendo-se manualmente (control.move)",
-        "nl": "Handmatig bewegen (control.move)",
-        "fr": "Se déplacer manuellement (control.move)",
-        "it": "Muoversi manualmente (control.move)",
-        "es": "Moviéndose manualmente (control.move)",
-        "pl": "Ręczne poruszanie się (control.move)",
-        "zh-cn": "手动移动 (control.move)"
+        "de": "Manuelles steuern (control.move)"
     },
     "feature.control.spotArea_cleanings": {
         "en": "Number of spot area cleanings (control.spotArea_cleanings)",
-        "de": "Anzahl Bereichsreinigungen (control.spotArea_cleanings)",
-        "ru": "Количество точечных уборок (control.spotArea_cleanings)",
-        "pt": "Número de limpezas de área local (control.spotArea_cleanings)",
-        "nl": "Aantal schoonmaakbeurten ter plaatse (control.spotArea_cleanings)",
-        "fr": "Nombre de nettoyages localisés (control.spotArea_cleanings)",
-        "it": "Numero di pulizie dell'area spot (control.spotArea_cleanings)",
-        "es": "Número de limpiezas de áreas puntuales (control.spotArea_cleanings)",
-        "pl": "Liczba miejscowych czyszczeń (control.spotArea_cleanings)",
-        "zh-cn": "现场清洁次数 (control.spotArea_cleanings)"
+        "de": "Anzahl Bereichsreinigungen (control.spotArea_cleanings)"
     },
     "feature.map.virtualBoundaries": {
         "en": "Create states for virtual boundaries",
@@ -240,38 +100,14 @@ systemDictionary = {
     "featuresWarning": {
         "en": "Warning: Unlocking of functions is at your own risk. The implementation of functions varies from model to model and some functions are not available in every model.",
         "de": "Warnung: Das Freischalten von Funktionen geschieht auf eigene Gefahr. Die Implementierungen von Funktionen sind von Modell zu Modell unterschiedlich und einige Funktionen sind nicht bei jedem Modell verfügbar.",
-        "ru": "Предупреждение: разблокировка функций на свой страх и риск. Реализация функций варьируется от модели к модели, и некоторые функции доступны не в каждой модели.",
-        "pt": "Aviso: O desbloqueio de funções é por sua conta e risco. A implementação de funções varia de modelo para modelo e algumas funções não estão disponíveis em todos os modelos.",
-        "nl": "Waarschuwing: het ontgrendelen van functies is op eigen risico. De implementatie van functies verschilt van model tot model en sommige functies zijn niet in elk model beschikbaar.",
-        "fr": "Avertissement: le déverrouillage des fonctions est à vos risques et périls. L'implémentation des fonctions varie d'un modèle à l'autre et certaines fonctions ne sont pas disponibles dans tous les modèles.",
-        "it": "Avvertenza: lo sblocco delle funzioni è a proprio rischio. L'implementazione delle funzioni varia da modello a modello e alcune funzioni non sono disponibili in tutti i modelli.",
-        "es": "Advertencia: el desbloqueo de funciones es bajo su propio riesgo. La implementación de funciones varía de un modelo a otro y algunas funciones no están disponibles en todos los modelos.",
-        "pl": "Ostrzeżenie: Odblokowanie funkcji odbywa się na własne ryzyko. Implementacja funkcji różni się w zależności od modelu, a niektóre funkcje nie są dostępne w każdym modelu.",
-        "zh-cn": "警告：解锁功能需要您自担风险。函数的实现因模型而异，并且某些函数并非在每个模型中都可用。"
     },
     "workaround.batteryValue": {
         "en": "Enable workaround for weird battery values (e.g. on Deebot 900/901)",
         "de": "Workaround für merkwürdigen Verlauf der Akkuwerte (z.B. beim Deebot 900/901)",
-        "ru": "Включить обходной путь для странных значений батареи (например, на Deebot 900/901)",
-        "pt": "Ative a solução alternativa para valores estranhos da bateria (por exemplo, no Deebot 900/901)",
-        "nl": "Tijdelijke oplossing inschakelen voor rare batterijwaarden (bijvoorbeeld op Deebot 900/901)",
-        "fr": "Activer la solution de contournement pour les valeurs de batterie étranges (par exemple sur Deebot 900/901)",
-        "it": "Abilita soluzione alternativa per strani valori della batteria (ad es. Su Deebot 900/901)",
-        "es": "Habilite la solución alternativa para valores de batería extraños (por ejemplo, en Deebot 900/901)",
-        "pl": "Włącz obejście dziwnych wartości baterii (np. W Deebot 900/901)",
-        "zh-cn": "启用针对电池电量异常的解决方法（例如在Deebot 900/901上）"
     },
     "workaround.lastCleaningAPICall": {
-        "en": "Use alternative API call for lastCleaningMapImageURL and lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "de": "Alternativen API Befehl für lastCleaningMapImageURL und lastCleaningTimestamp nutzen (Ozmo 920/950/T8)",
-        "ru": "Используйте альтернативный вызов API для lastCleaningMapImageURL и lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "pt": "Use a chamada de API alternativa para lastCleaningMapImageURL e lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "nl": "Gebruik een alternatieve API-aanroep voor lastCleaningMapImageURL en lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "fr": "Utiliser un autre appel d'API pour lastCleaningMapImageURL et lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "it": "Utilizza una chiamata API alternativa per lastCleaningMapImageURL e lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "es": "Utilice una llamada API alternativa para lastCleaningMapImageURL y lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "pl": "Użyj alternatywnego wywołania API dla lastCleaningMapImageURL i lastCleaningTimestamp (Ozmo 920/950/T8)",
-        "zh-cn": "对lastCleaningMapImageURL和lastCleaningTimestamp使用替代API调用（Ozmo 920/950）"
+        "en": "Use alternative API call for lastCleaningMapImageURL and lastCleaningTimestamp (OZMO 920/950/T8)",
+        "de": "Alternativen API Befehl für lastCleaningMapImageURL und lastCleaningTimestamp nutzen (OZMO 920/950/T8)"
     },
     "feature.pauseBeforeDockingChargingStation.areasize": {
         "en": "Area size for \"pauseBeforeDockingChargingStation\"",
@@ -303,14 +139,6 @@ systemDictionary = {
     },
     "pollingInterval": {
         "en": "Polling interval (seconds)",
-        "de": "Abfrage Intervall (Sekunden)",
-        "ru": "Интервал опроса (секунды)",
-        "pt": "Intervalo de pesquisa (segundos)",
-        "nl": "Polling-interval (seconden)",
-        "fr": "Intervalle d'interrogation (secondes)",
-        "it": "Intervallo di polling (secondi)",
-        "es": "Intervalo de sondeo (segundos)",
-        "pl": "Interwał odpytywania (sekundy)",
-        "zh-cn": "轮询间隔（秒）"
+        "de": "Abfrage Intervall (Sekunden)"
     }
 };
