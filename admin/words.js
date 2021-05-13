44ddadee076df6937e2c056048750bf6fbcb3dbb
mrbungle64 <mrb1232@posteo.de>
2021-05-13 11:53:24 +0200

    Add russian translation

--- admin/words.js
+++ admin/words.js
@@ -7,138 +7,172 @@
 systemDictionary = {
     "email": {
         "en": "Email or Ecovacs ID",
-        "de": "E-Mail oder Ecovacs-ID"
+        "de": "E-Mail oder Ecovacs-ID",
+        "ru": "Электронная почта или идентификатор Ecovacs"
     },
     "password": {
         "en": "Password",
-        "de": "Passwort"
+        "de": "Passwort",
+        "ru": "Пароль"
     },
     "countrycode": {
         "en": "Country",
-        "de": "Land"
+        "de": "Land",
+        "ru": "Страна"
     },
     "deviceNumber": {
         "en": "Device number",
-        "de": "Gerät Nr."
+        "de": "Gerät Nr.",
+        "ru": "Номер устройства"
     },
     "english": {
         "en": "English",
-        "de": "Englisch"
+        "de": "Englisch",
+        "ru": "английский"
     },
     "german": {
         "en": "German",
-        "de": "Deutsch"
+        "de": "Deutsch",
+        "ru": "Немецкий"
     },
     "pre-selection": {
         "en": "pre-selection",
-        "de": "Vorauswahl verwenden"
+        "de": "Vorauswahl verwenden",
+        "ru": "предварительный отбор"
     },
     "enable": {
         "en": "enable",
-        "de": "aktivieren"
-    },
-    "areas": {
-        "en": "areas",
-        "de": "Bereiche"
-    },
-    "standardFunctions": {
-        "en": "Standard functions",
-        "de": "Standard-Funktionen"
-    },
-    "other ...": {
-        "en": "Other ...",
-        "de": "Weiteres ..."
+        "de": "aktivieren",
+        "ru": "включить"
     },
     "disable": {
         "en": "disabled",
-        "de": "deaktivieren"
+        "de": "deaktivieren",
+        "ru": "запрещать"
+    },
+    "areas": {
+        "en": "areas",
+        "de": "Bereiche",
+        "ru": "области"
+    },
+    "standardFunctions": {
+        "en": "Standard functions",
+        "de": "Standard-Funktionen",
+        "ru": "Стандартные функции"
+    },
+    "other ...": {
+        "en": "Other ...",
+        "de": "Weiteres ...",
+        "ru": "Другие ..."
     },
     "feature.cleaninglog.channel": {
         "en": "Cleaning logs (\"cleaninglog\" channel)",
-        "de": "Reinigungsprotokolle (\"cleaninglog\" Kanal)"
+        "de": "Reinigungsprotokolle (\"cleaninglog\" Kanal)",
+        "ru": "Журналы очистки (\"cleaninglog\" канал)"
     },
     "feature.control.pause": {
         "en": "Pause function (\"control.pause\")",
-        "de": "Pause-Funktion (\"control.pause\")"
+        "de": "Pause-Funktion (\"control.pause\")",
+        "ru": "Функция паузы (\"control.pause\")"
     },
     "feature.control.resume": {
         "en": "Resume function (\"control.resume\")",
-        "de": "Fortsetzen-Funktion (\"control.resume\")"
+        "de": "Fortsetzen-Funktion (\"control.resume\")",
+        "ru": "Функция возобновления (\"control.resume\")"
     },
     "feature.control.playSound": {
         "en": "Play a sound for locating the device (\"control.playSound\")",
-        "de": "Sound zur Lokalisierung des Roboters (\"control.playSound\")"
+        "de": "Sound zur Lokalisierung des Roboters (\"control.playSound\")",
+        "ru": "Воспроизвести звук для определения местоположения устройства (\"control.playSound\")"
     },
     "feature.control.playIamHere": {
         "en": "Play \"I am here\" for locating the device (\"control.playIamHere\")",
-        "de": "\"Ich bin hier\"-Ausgabe zur Lokalisierung des Roboters (\"control.playIamHere\")"
+        "de": "\"Ich bin hier\"-Ausgabe zur Lokalisierung des Roboters (\"control.playIamHere\")",
+        "ru": "Играйте «Я здесь», чтобы найти устройство (\"control.playIamHere\")"
     },
     "feature.info.dustbox": {
         "en": "Display dustbox status (\"info.dustbox\")",
-        "de": "Zeige an, ob die Staubbox eingesetzt ist (\"info.dustbox\")"
+        "de": "Zeige an, ob die Staubbox eingesetzt ist (\"info.dustbox\")",
+        "ru": "Отображение статуса пылесборника (\"info.dustbox\")"
     },
     "feature.control.move": {
         "en": "Manually moving around (\"control.move\")",
-        "de": "Manuelle Steuerungsbefehle (\"control.move\")"
+        "de": "Manuelle Steuerungsbefehle (\"control.move\")",
+        "ru": "Перемещение вручную (\"control.move\")"
     },
     "feature.map.virtualBoundaries": {
         "en": "Create states for virtual boundaries and no mop zones",
         "de": "Erzeuge Datenpunkte für virtuelle Begrenzungen und No-Mop-Zones",
+        "ru": "Создавайте состояния для виртуальных границ"
     },
     "feature.map.virtualBoundaries.write": {
         "en": "Delete, save and recreate saved virtual boundaries and no mop zones",
         "de": "Löschen, Speichern und Wiederherstellen von gespeicherten virtuellen Begrenzungen und No-Mop-Zones",
+        "ru": "Удалить, сохранить и воссоздать сохраненные виртуальные границы"
     },
     "featuresWarning": {
-        "en": "Warning: Unlocking of functions is at your own risk. The implementation of functions varies from model to model and some functions are not available in every model.",
-        "de": "Warnung: Das Freischalten von Funktionen geschieht auf eigene Gefahr. Die Implementierungen von Funktionen sind von Modell zu Modell unterschiedlich und einige Funktionen sind nicht bei jedem Modell verfügbar.",
+        "en": "Warning: Unlocking of functions is at your own risk. The implementation of functions varies from model to model and some functions are not available in every model",
+        "de": "Warnung: Das Freischalten von Funktionen geschieht auf eigene Gefahr. Die Implementierungen von Funktionen sind von Modell zu Modell unterschiedlich und einige Funktionen sind nicht bei jedem Modell verfügbar",
+        "ru": "Предупреждение: Вы можете разблокировать функции на свой страх и риск. Реализация функций варьируется от модели к модели, и некоторые функции доступны не в каждой модели"
     },
     "workaround.batteryValue": {
         "en": "Enable workaround for weird battery values (e.g. on Deebot 900/901)",
         "de": "Workaround für merkwürdigen Verlauf der Akkuwerte aktivieren (z.B. beim Deebot 900/901)",
+        "ru": "Предотвращение странных значений заряда батареи (например, на Deebot 900/901)"
     },
     "feature.pauseBeforeDockingChargingStation.areasize": {
         "en": "Area size (\"control.extended.pauseBeforeDocking[...]\")",
-        "de": "Größe des Bereichs (\"control.extended.pauseBeforeDocking[...]\")"
+        "de": "Größe des Bereichs (\"control.extended.pauseBeforeDocking[...]\")",
+        "ru": "Размер области (\"control.extended.pauseBeforeDocking[...]\")"
     },
     "feature.control.spotAreaSync": {
         "en": "Synchronize spotArea buttons in the \"control\" channel and also the labels",
-        "de": "\"spotArea\" Buttons im \"control\" Kanal und die Beschriftung der Bereiche synchronisieren"
+        "de": "\"spotArea\" Buttons im \"control\" Kanal und die Beschriftung der Bereiche synchronisieren",
+        "ru": "Синхронизируйте кнопки spotArea в \"control\" канале, а также ярлыки"
     },
     "feature.map.spotAreas.cleanSpeed": {
         "en": "Option to control clean speed separately for each spot area",
-        "de": "Option zur separaten Steuerung der Reinigungsstärke für die einzelnen Bereiche"
+        "de": "Option zur separaten Steuerung der Reinigungsstärke für die einzelnen Bereiche",
+        "ru": "Возможность управления скоростью уборки отдельно для каждой области пятна"
     },
     "feature.map.spotAreas.waterLevel": {
         "en": "Option to control water level separately for each spot area",
-        "de": "Option zur separaten Steuerung der Wasserzufuhr-Stärke für die einzelnen Bereiche"
+        "de": "Option zur separaten Steuerung der Wasserzufuhr-Stärke für die einzelnen Bereiche",
+        "ru": "Возможность контролировать уровень воды отдельно для каждого участка"
     },
     "feature.control.experimental": {
         "en": "Some further experimental features (\"control.extended\" channel)",
-        "de": "Einige weitere experimentelle Features (\"control.extended\" Kanal)"
+        "de": "Einige weitere experimentelle Features (\"control.extended\" Kanal)",
+        "ru": "Некоторые дополнительные экспериментальные функции (\"control.extended\" канал)"
     },
     "languageForSpotAreaNames": {
         "en": "Language for spot area labels",
-        "de": "Sprache für die Beschriftung der Bereiche"
+        "de": "Sprache für die Beschriftung der Bereiche",
+        "ru": "Язык меток областей пятна"
     },
     "workaround.suppressUnknownCurrentSpotArea": {
         "en": "Suppress \"unknown\" value for \"map.deebotPositionCurrentSpotAreaID\" state",
-        "de": "Wert \"unknown\" im Datenpunkt \"map.deebotPositionCurrentSpotAreaID\" unterdrücken"
+        "de": "Wert \"unknown\" im Datenpunkt \"map.deebotPositionCurrentSpotAreaID\" unterdrücken",
+        "ru": "Подавить \"unknown\" значение для состояния \"map.deebotPositionCurrentSpotAreaID\""
     },
     "doNotSynchronize": {
         "en": "Do not synchronize",
-        "de": "Nicht synchronisieren"
+        "de": "Nicht synchronisieren",
+        "ru": "Не синхронизировать"
     },
     "onlyCreate": {
         "en": "Only create new ones",
-        "de": "Nur neue erstellen"
+        "de": "Nur neue erstellen",
+        "ru": "Создавайте только новые"
     },
     "fullSynchronization": {
         "en": "Full synchronization",
-        "de": "Voll synchronisieren"
+        "de": "Voll synchronisieren",
+        "ru": "Полная синхронизация"
     },
     "pollingInterval": {
         "en": "Polling interval (seconds)",
-        "de": "Abfrage-Intervall (Sekunden)"
+        "de": "Abfrage-Intervall (Sekunden)",
+        "ru": "Интервал опроса (секунды)"
     }
 };
