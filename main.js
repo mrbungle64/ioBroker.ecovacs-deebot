c9cf3b6a1f729de014acdbd12829cb977b9c4b33
mrbungle64 <mrb1232@posteo.de>
2024-01-07 22:01:11 +0100

    Add additional air quality data ('pm_10' and 'voc_num'). Usage currently unknown.

--- main.js
+++ main.js
@@ -1078,6 +1078,21 @@ class EcovacsDeebot extends utils.Adapter {
                                 await this.setStateConditionalAsync(
                                     'info.airQuality.humidity', humidity, true);
                             })();
+                            // Additional values (Usage currently unknown)
+                            if (object['voc_num'] !== undefined) {
+                                this.createObjectNotExists(
+                                    'info.airQuality.pm_10', 'pm_10 (usage currently unknown)',
+                                    'number', 'value', false, 0, '').then(() => {
+                                    this.setStateConditional('info.airQuality.pm_10', object['pm_10'], true);
+                                });
+                            }
+                            if (object['voc_num'] !== undefined) {
+                                this.createObjectNotExists(
+                                    'info.airQuality.voc_num', 'voc_num (usage currently unknown)',
+                                    'number', 'value', false, 0, '').then(() => {
+                                    this.setStateConditional('info.airQuality.volatileOrganicCompounds', object['voc_num'], true);
+                                });
+                            }
                         });
                     });
 
